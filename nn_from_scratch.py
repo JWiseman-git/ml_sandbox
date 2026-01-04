597beb3a9c197966144c898c6b3acf86b26c7b65
Jordan Wiseman <74243511+JWiseman-git@users.noreply.github.com>
2026-01-04 13:59:34 -0500

    latest

---
+++ nn_from_scratch.py
@@ -0,0 +1,10 @@
+# simple mnist nn from scratch - some basics refreshing
+# uses mnist digit detection 
+# notes for this - equations etc - in obisidian
+
+import numpy as np 
+import pandas as pd 
+
+# ten possible classes (0...9)
+# where y is the expected output NEED TO COVER A MONITORING FRAMEWORK AND ADD TO CV 
+
